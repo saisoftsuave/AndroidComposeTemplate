480fbdedb49be076ee8433f2112cc0472e76a377
saisoftsuave <sai.babu@softsuave.org>
2025-04-29 19:42:15 +0530

    added git ignore and api constents

--- core/network/src/main/java/com/example/network/ApiContants.kt
+++ core/network/src/main/java/com/example/network/ApiContants.kt
@@ -1,4 +1,4 @@
 package com.example.network
 
-private const val BASE_URL =
-    "https://ef27-101-0-63-55.ngrok-free.app/"
+internal const val BASE_URL =
+    "https://d53a-182-75-137-154.ngrok-free.app/"
